589f856e3bb4f6b23801279a141595a72db800d0
tavvarez <gabrieldaraujotavares@gmail.com>
2024-01-11 21:45:14 -0300

    FEAT: dentista repository

---
+++ src/main/java/image/dente/api/dentista/DentistaRepository.java
@@ -0,0 +1,11 @@
+package image.dente.api.dentista;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DentistaRepository extends JpaRepository<Dentista, Long> {
+
+    Page<Dentista> findAllByAtivoTrue(Pageable paginacao);
+
+}
