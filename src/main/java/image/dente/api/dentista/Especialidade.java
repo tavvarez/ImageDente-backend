b79de9e58ecff47591c9b1e4770580f883553648
tavvarez <gabrieldaraujotavares@gmail.com>
2024-01-11 21:45:24 -0300

    FEAT: especialidades do dentista

---
+++ src/main/java/image/dente/api/dentista/Especialidade.java
@@ -0,0 +1,10 @@
+package image.dente.api.dentista;
+
+public enum Especialidade {
+    ORTODONTISTA,
+    ODONTOPEDIATRA,
+    GERAL,
+    IMPLANTODENTISTA,
+    ESTOMATOLOGISTA,
+    PERIODONTISTA,
+}
