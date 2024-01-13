9f1e56b2e5165b7f74ad7d2c76d3e071a9943224
tavvarez <gabrieldaraujotavares@gmail.com>
2024-01-12 22:59:11 -0300

    tratamento de erro (exception)

---
+++ src/main/java/image/dente/api/infra/exception/TratamentoErro.java
@@ -0,0 +1,29 @@
+package image.dente.api.infra.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratamentoErro {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratamentoDeErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratamentoDeErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+}
