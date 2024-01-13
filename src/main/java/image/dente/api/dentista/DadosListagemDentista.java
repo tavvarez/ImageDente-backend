7e5afef824ef9d24e2900c2220c437a2df3209f2
tavvarez <gabrieldaraujotavares@gmail.com>
2024-01-12 23:00:03 -0300

    gitignore

--- src/main/java/image/dente/api/dentista/DadosListagemDentista.java
+++
@@ -1,9 +0,0 @@
-package image.dente.api.dentista;
-
-public record DadosListagemDentista(Long id, String nome, String email, String cro, Especialidade especialidade) {
-
-    public DadosListagemDentista(Dentista dentista) {
-        this(dentista.getId(), dentista.getNome(), dentista.getEmail(), dentista.getCro(), dentista.getEspecialidade());
-    }
-
-}
