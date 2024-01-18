721b40066cfaa78e74c46d16d7071c15db2c5adb
tavvarez <gabrieldaraujotavares@gmail.com>
2024-01-18 20:42:57 -0300

    Unused imports

--- src/main/java/image/dente/api/domain/paciente/PacienteRepository.java
+++ src/main/java/image/dente/api/domain/paciente/PacienteRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import image.dente.api.domain.paciente.Paciente;
-
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 }
